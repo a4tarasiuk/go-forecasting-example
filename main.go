c7d2cbaded9c0607f8577ef22d901fd8d032399a
a4tarasiuk <>
2024-08-24 18:17:26 +0300

    Dockerize application

--- main.go
+++ main.go
@@ -1,61 +1,11 @@
 package main
 
 import (
-	"fmt"
-
-	"forecasting/rules/persistence"
+	"forecasting/calculation/coordination"
 )
 
 func main() {
-	// startDate := carbon.Parse("2023-01-01").ToDateStruct()
-	// endDate := carbon.Parse("2023-01-01").ToDateStruct()
-	//
-	// lhm := carbon.Parse("2022-12-01").ToDateStruct()
-	//
-	// nMonths := 3
-	//
-	// forecastRule := &rules.ForecastRule{
-	// 	ID:                                   1,
-	// 	HomeOperators:                        []int{1},
-	// 	PartnerOperators:                     []int{2, 3},
-	// 	Period:                               types.NewPeriod(startDate, endDate),
-	// 	TrafficDirection:                     core.InboundTrafficDirection,
-	// 	ServiceType:                          core.VoiceMO,
-	// 	ForecastModel:                        core.ManualVolumeForecastModel,
-	// 	DistributionModel:                    core.MovingAverageDistributionModel,
-	// 	Volume:                               2500.0,
-	// 	DistributionModelMovingAverageMonths: &nMonths,
-	// 	LHM:                                  &lhm,
-	// }
+	calcCoordinator := coordination.NewForecastRuleCalculationCoordinator()
 
-	// manualVolumeModel := forecast_models.NewManualVolume()
-	//
-	// trafficRecords, err := manualVolumeModel.Calculate(forecastRule)
-	//
-
-	// movingAverageDistributionModel := distribution_models.NewMovingAverage()
-	//
-	// forecastRecords := []calculation.ForecastRecord{
-	// 	{VolumeActual: forecastRule.Volume, Month: carbon.Parse("2023-01-01").ToDateStruct()},
-	// }
-	//
-	// trafficRecords, err := movingAverageDistributionModel.Apply(forecastRule, forecastRecords)
-	//
-	// fmt.Println(err)
-	//
-	// totalForecastVolumeAfterDistribution := traffic.CalculateTotalHistoricalVolume(trafficRecords)
-	//
-	// fmt.Println("Total F volume", totalForecastVolumeAfterDistribution)
-	//
-	// for _, record := range trafficRecords {
-	// 	fmt.Printf("%+v\n", record)
-	// }
-
-	repo := persistence.NewPostgresForecastRuleRepository()
-
-	forecastRules := repo.GetMany()
-
-	for _, forecastRule := range forecastRules {
-		fmt.Printf("%+v\n\n", forecastRule)
-	}
+	calcCoordinator.CalculateAll()
 }
